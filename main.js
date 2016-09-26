984dc4eb8d2720dfb74b9641138b772c571be16d
jonathandavidblack <jonnybl915@gmail.com>
2016-09-26 10:17:12 -0400

    added event listeners

--- main.js
+++ main.js
@@ -14,3 +14,19 @@ var ulContainer = document.querySelector('ul')
 for (var i = 0; i < listOfNames.length; i = i + 1) {
   ulContainer.innerHTML += "<li>" + listOfNames[i] +"</li>"
 }
+
+var pTagSentenceExample = document.querySelector('.paragraphs')
+var destinationElement = document.querySelector('h4 strong')
+if (pTagSentenceExample.textContent.length > 40) {
+  destinationElement.innerHTML = "This sentence is SOO Long!!!!"
+
+} else {
+  destinationElement.innerHTML = "Not too Long. Eat more green beans."
+}
+
+var btnElement = document.querySelector('button')
+var containerElement = document.querySelector('.container')
+
+var destroyEverything = function () {
+  containerEl.innerHTML = "<h3><mark> I told you not to push<br>I told you not to push<br>I told you not to push<mark><h3>"
+}
