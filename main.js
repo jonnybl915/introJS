978e26ac9d516bd00c387022f8be90950e8efb7c
jonathandavidblack <jonnybl915@gmail.com>
2016-09-26 10:02:34 -0400

    added a list of names to the page Dynamically using main.js file

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 document.querySelector('.title').innerHTML = "Welcome to JS"
+
+
+var listOfNames = [
+  "Phil",
+  "Jill",
+  "Hill",
+  "Will",
+  "Bill",
+  "Gill"
+]
+var ulContainer = document.querySelector('ul')
+
+for (var i = 0; i < listOfNames.length; i = i + 1) {
+  ulContainer.innerHTML += "<li>" + listOfNames[i] +"</li>"
+}
